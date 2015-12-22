d8914385ee35f1ce7ef356aed9e6127985ce79b4
parker <parker.garrison.ctr@navy.mil>
2015-12-22 14:21:20 -0500

    Modify controller with session data and add to eventdetails html page.

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -2,7 +2,44 @@
 
 eventsApp.controller('EventController',
 		function EventController($scope) {
-	
+		
+		$scope.event = {
+				name: 'Angular boot Camp',
+				date: '1/1/2016',
+				time: '12:30 am',
+				location: {
+					address: 'Cecil',
+					city: 'Jacksonville',
+					province: 'FL'
+				},	
+				imageUrl: '/img/angularjs-logo.png',
+				sessions: [
+				       {
+				    	   		name: 'Directives Masterclass',
+				    	   		creatorName: 'Parker',
+				    	   		duration: '1h',
+				    		    level: 'Advanced',
+				    		    abstract: 'Blah Blah Blah',
+				    		    upVoteCount: 0
+				       },
+				       {
+				    	   name: 'Controllers',
+			    	   		creatorName: 'Parker',
+			    	   		duration: '1h',
+			    		    level: 'beginner',
+			    		    abstract: 'Blooh Blooh Blooh Blooh',
+			    		    upVoteCount: 0
+				       },
+				       {
+				    	   name: 'Directives Part 2',
+			    	   		creatorName: 'Parker',
+			    	   		duration: '2h',
+			    		    level: 'Advanced',
+			    		    abstract: 'Blah Blah Blah',
+			    		    upVoteCount: 0	
+				       }
+				]
+		}
 	
 		}
 );
